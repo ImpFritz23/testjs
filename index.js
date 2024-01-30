be953e9bcb132d7eaafacc9518e5ece2677407c0
fritz <imperialfritz@gmail.com>
2024-01-30 09:40:15 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+
+
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Restful API Backend Using ExpressJS' });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
